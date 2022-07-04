cec9300c0e7718c53f172116b4004c0de1125ce1
Johanne Carlos <johanneCarlosrosa13@gmail.com>
2022-07-04 18:43:28 -0300

    first commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+  console.log(`Passo ${c}`)
+  c++
+} while (c <=6)
+
+/* Recomendada. while == enquanto
+do while == faça enquanto
+var c = 1
+while (c <= 500) {
+  console.log(`passo ${c}`)
+  c++
+}
+*/
+
+/* A pior forma de escalar
+console.log('Tudo bem?) 
+console.log('Tudo bem?) 
+console.log('Tudo bem?) 
+console.log('Tudo bem?) 
+console.log('Tudo bem?) 
+console.log('Tudo bem?) 
+*/
